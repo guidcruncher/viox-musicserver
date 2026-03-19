32402908315b7aae965c4d57909a8db9b12e85ab
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-03-19 12:51:04 +0000

    Scaffolding

--- src/routes/speakerRoutes.ts
+++ src/routes/speakerRoutes.ts
@@ -4,13 +4,41 @@ import type { VioxBackend } from "@/types"
 
 export function registerSpeakerRoutes(app: FastifyInstance, backend: VioxBackend) {
   app.get("/speakers", async (_req, res) => {
-    const speakers = await backend.speakers.list()
+    const speakers = await backend.speakers.getAllSpeakers()
     res.send(speakers)
   })
 
-  app.post("/speakers/select", async (req, res) => {
-    const body = req.body as { id: string }
-    await backend.speakers.select(body.id)
+  app.post("/speakers/mute", async (req, res) => {
+    await backend.speakers.muteAll()
+    res.send({ ok: true })
+  })
+
+  app.post("/speakers/unmute", async (req, res) => {
+    await backend.speakers.unmuteAll()
+    res.send({ ok: true })
+  })
+
+  app.post("/speakers/:volume", async (req, res) => {
+    const { volume } = req.params as any
+    await backend.speakers.setVolumeAll(volume)
+    res.send({ ok: true })
+  })
+
+  app.post("/speakers/:id/mute", async (req, res) => {
+    const { id } = req.params as any
+    await backend.speakers.mute(id)
+    res.send({ ok: true })
+  })
+
+  app.post("/speakers/:id/unmute", async (req, res) => {
+    const { id } = req.params as any
+    await backend.speakers.unmute(id)
+    res.send({ ok: true })
+  })
+
+  app.post("/speakers/:id/:volume", async (req, res) => {
+    const { id, volume } = req.params as any
+    await backend.speakers.setVolume(id, volume)
     res.send({ ok: true })
   })
 }
