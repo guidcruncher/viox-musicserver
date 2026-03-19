782098352e101758f0c89fb43f4322ae2cc97c6c
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-03-19 13:01:41 +0000

    Scaffolding

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,6 @@ import { registerEqualizerRoutes } from "@/routes/equalizerRoutes";
 import { registerImportRoutes } from "@/routes/importRoutes";
 import { registerLibraryRoutes } from "@/routes/libraryRoutes";
 import { registerPlaybackRoutes } from "@/routes/playbackRoutes";
-import { registerSearchRoutes } from "@/routes/searchRoutes";
 import { registerSpeakerRoutes } from "@/routes/speakerRoutes";
 
 
@@ -19,6 +18,5 @@ export const registerAllRoutes = async(app: FastifyInstance, backend: VioxBacken
   registerImportRoutes(app, backend);
   registerLibraryRoutes(app, backend);
   registerPlaybackRoutes(app, backend);
-  registerSearchRoutes(app, backend);
   registerSpeakerRoutes(app, backend);
 }
