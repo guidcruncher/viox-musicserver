424b0c37622e31136486f76354daf93de32da7f3
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-04-14 16:53:37 +0100

    Scaffolding

--- src/infra/networking/hashFilename.ts
+++ src/infra/networking/hashFilename.ts
@@ -4,11 +4,11 @@ import path from "path"
 import { getConfig } from "@/config"
 
 export const hashAudioFilename = (url: string): string => {
-     const hash = crypto.createHash("sha256").update(url).digest("hex")
-    return path.join( getConfig("cacheFolder"), `proxy_cache_${hash}.mp3`)
-  }
+  const hash = crypto.createHash("sha256").update(url).digest("hex")
+  return path.join(getConfig("cacheFolder"), `proxy_cache_${hash}.mp3`)
+}
 
 export const hashImageFilename = (url: string): string => {
-     const hash = crypto.createHash("sha256").update(url).digest("hex")
-    return path.join( getConfig("cacheFolder"), `albumart_cache_${hash}.mp3`)
-  }
+  const hash = crypto.createHash("sha256").update(url).digest("hex")
+  return path.join(getConfig("cacheFolder"), `albumart_cache_${hash}.mp3`)
+}
