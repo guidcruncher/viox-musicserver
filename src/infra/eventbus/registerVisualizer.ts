46b3a546b8c53e2ce40a3269c8608f67147917a0
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-04-19 09:14:17 +0100

    visualizeer

---
+++ src/infra/eventbus/registerVisualizer.ts
@@ -0,0 +1,37 @@
+import fastifyWebsocket from "@fastify/websocket" // Import the plugin correctly
+import { FastifyInstance } from "fastify"
+
+import { logger } from "@/logger"
+
+import { AudioService } from "@/services/fftService"
+
+export async function registerVisualizer(fastify: FastifyInstance) {
+
+  // 2. Define the WebSocket route
+  // We use the 'connection' object which contains the raw socket
+  fastify.get("/api/ws/visualizer", { websocket: true }, (connection) => {
+    const socket = connection
+    const audioService = AudioService.getInstance()
+
+    const onData = (fftData: Float32Array) => {
+        if (socket.readyState === 1) { 
+          socket.send(fftData.buffer)
+        }
+      }
+
+      logger.info("Visualizer: New client subscribed")
+      audioService.addListener(onData)
+
+      socket.on("close", () => {
+        logger.info("Visualizer: Client unsubscribed")
+        audioService.removeListener(onData)
+      })
+
+      socket.on("error", (err: Error) => {
+        logger.error("Visualizer socket error", err)
+        audioService.removeListener(onData)
+      })
+	
+  })
+
+}
